97c88a47b60d7680157e8a344566f80aa9935ed6
XRISTECH <oluebube2015@gmail.com>
2025-03-06 12:44:49 +0100

    first commit

---
+++ lib/cores/app_exports.dart
@@ -0,0 +1,35 @@
+export 'package:flutter/material.dart';
+
+// export 'package:electronicsshop_app/presentation/onboarding/onboarding.dart';
+// export 'package:electronicsshop_app/presentation/onboarding/onboarding1.dart';
+// export 'package:electronicsshop_app/presentation/onboarding/onboarding2.dart';
+export 'package:electronicshopadmin_app/presentation/splash_screen/splash.dart';
+// export 'package:electronicsshop_app/presentation/auth/login.dart';
+// export 'package:electronicsshop_app/presentation/auth/sign_up.dart';
+// export 'package:electronicsshop_app/routes/app_routes.dart';
+// export 'package:electronicsshop_app/cores/utils/navigator_service.dart';
+// export 'package:electronicsshop_app/presentation/auth/authenticate.dart';
+// export 'package:electronicsshop_app/cores/constants.dart';
+// export 'package:electronicsshop_app/cores/utils/image_constants.dart';
+// export 'package:electronicsshop_app/cores/utils/validation_functions.dart';
+// export  'package:electronicsshop_app/presentation/appmain_screen/home/app_homescreen.dart';
+// export 'package:electronicsshop_app/presentation/appbanner/appbanner.dart';
+// export 'package:electronicsshop_app/data/models.dart';
+// export 'package:electronicsshop_app/presentation/appmain_screen/home/home_shared/home_shared.dart';
+// export 'package:electronicsshop_app/presentation/appmain_screen/home_controller.dart';
+// export 'package:electronicsshop_app/presentation/appmain_screen/notification/notification.dart';
+// export 'package:electronicsshop_app/presentation/appmain_screen/profile/profile.dart';
+// export 'package:electronicsshop_app/presentation/appmain_screen/search/search.dart';
+// export 'package:electronicsshop_app/data/repository/auth_repository.dart';
+// export 'package:electronicsshop_app/presentation/wrapper/wrapper.dart';
+// export 'package:electronicsshop_app/presentation/loading/loading.dart';
+// export '../presentation/appmain_screen/products_details/products_details.dart';
+// export 'package:electronicsshop_app/presentation/appmain_screen/products_details/shared/product_details_shared.dart';
+// export 'package:electronicsshop_app/providers/cart_provider.dart';
+// export 'package:electronicsshop_app/presentation/appmain_screen/cart_screen.dart';
+//packages
+// export 'package:shared_preferences/shared_preferences.dart';
+// export 'package:electronicsshop_app/data/repository/local_storage_repository.dart';
+// export 'package:flutter_riverpod/flutter_riverpod.dart';
+// export 'package:http/http.dart';
+export 'dart:convert';
